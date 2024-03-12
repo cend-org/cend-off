2552657675ea446e423c3fa0770e935253ee8e57
Julis <julis@thegreenshot.green>
2024-03-12 14:14:11 +0100

    Correction de deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,8 @@ cd /workspace/duval && sudo git reset --hard origin/master && sudo git fetch ori
 echo "Building the application ... "
 cd /workspace/duval/cmd/app && go build -buildvcs=false
 
-# Check if the build was successful
-# shellcheck disable=SC2181
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to build the application."
-    exit 1
-fi
-
-echo "Reloading systemd ..."
 echo "Reloading systemd..."
 sudo systemctl daemon-reload
-
 
 echo "Restarting the duval service..."
 sudo systemctl restart duval
