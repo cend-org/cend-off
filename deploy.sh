816b36c0e770e17d438ef0a06009171406b5be13
Julis <julis@thegreenshot.green>
2024-03-12 13:43:38 +0100

    Correction de deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Updating code from Git..."
 cd /workspace/duval && sudo git reset --hard origin/master && sudo git fetch origin
 
 echo "Building the application ... "
-cd /workspace/duval/cmd/app && go build go build -buildvcs=false
+cd /workspace/duval/cmd/app && go build -buildvcs=false
 
 # Check if the build was successful
 # shellcheck disable=SC2181
