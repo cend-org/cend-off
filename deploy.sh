b8f44b5aa78125a1a011df411e01860434ccc8cc
Julis <julis@thegreenshot.green>
2024-05-08 14:13:54 +0200

    ajout de la table user_course_preference

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Updating code from Git..."
-cd /workspace/duval && sudo git fetch origin && sudo git reset --hard origin/master
+cd /usr/workspace/duval && sudo git fetch origin && sudo git reset --hard origin/master
 
-echo "Building the application ... "
-cd /workspace/duval/cmd/app && go build -buildvcs=false
+echo "building..."
+cd /usr/workspace/duval/cmd/app && go build -buildvcs=false
 
 echo "Reloading systemd..."
 sudo systemctl daemon-reload
 
 echo "Restarting the duval service..."
-sudo systemctl restart duval
+sudo systemctl restart cend
 
 echo "Deployment completed successfully."
