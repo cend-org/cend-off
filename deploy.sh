c784e607a2b0183fa58daf69a5e7934970e7e839
Julis <julis@thegreenshot.green>
2024-05-13 10:31:34 +0200

    remove configuration.sh and merge content to the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#sudo apt update
+#sudo apt install -y golang
+#go version
+#sudo apt install git
+#sudo git clone https://github.com/cend-org/duval.git
+#sudo nano /etc/systemd/system/cend.service
+#sudo systemctl daemon-reload
+#sudo systemctl enable cend.service
+#sudo systemctl start cend.service
+#sudo systemctl restart cend.service
+#sudo systemctl status cend.service
+#to reload the service
+#sudo systemctl daemon-reload
+#https://medium.com/@olayinkancs/deploying-golang-application-to-aws-ec2-instance-d26891d25b2e
+
 echo "Updating code from Git..."
 cd /usr/workspace/duval && sudo git fetch origin && sudo git reset --hard origin/master
 
