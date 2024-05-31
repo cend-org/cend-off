098233cc02d462878c3f0c37168a32a435825a17
ValicioKely <razalicio008@gmail.com>
2024-05-31 22:50:26 +0300

    Refactor resolvers

--- graph/resolver/resolver.go
+++ graph/resolver/resolver.go
@@ -1,12 +1,26 @@
 package resolver
 
-import "github.com/cend-org/duval/graph/generated"
+import (
+	"github.com/cend-org/duval/graph/generated"
+	"github.com/cend-org/duval/pkg/academic"
+	usr "github.com/cend-org/duval/pkg/user"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct{}
+
+type mutationResolver struct {
+	*usr.UserMutation
+	*academic.AcademicMutation
+}
+
+type queryResolver struct {
+	*usr.UserQuery          `json:"*Usr.Query,omitempty"`
+	*academic.AcademicQuery `json:"*Academic.Query,omitempty"`
+}
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{} }
