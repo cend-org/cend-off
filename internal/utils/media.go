2523911365e932a0c3533ef50c5ca22b8ebf43b8
ValicioKely <razalicio008@gmail.com>
2024-06-07 08:02:32 +0300

    Add extra mimetype

--- internal/utils/media.go
+++ internal/utils/media.go
@@ -17,16 +17,63 @@ func IsValidDocument(mType string) bool {
 }
 
 func IsValidImage(mType string) bool {
-	allowed := []string{"image/png", "image/jpeg"}
+	allowed := []string{
+		"image/png",
+		"image/jpeg",
+		"image/jpg",
+		"image/gif",
+		"image/bmp",
+		"image/tiff",
+		"image/webp",
+		"image/svg",
+		"image/x-ico",
+		"image/heic",
+	}
 	return mimetype.EqualsAny(mType, allowed...)
 }
 
 func IsValidVideo(mType string) bool {
-	allowed := []string{"video/mpeg", "video/mp4"}
+	allowed := []string{
+		"video/mpeg",
+		"video/mp4",
+		"video/x-msvideo",  //avi
+		"video/x-matroska", //mkv
+		"video/quicktime",  //mov
+		"video/x-ms-wmv",   //wmv
+		"video/x-flv",      //flv
+		"video/webm",       //webm
+		"video/mpeg",       //mpeg
+	}
 	return mimetype.EqualsAny(mType, allowed...)
 }
 
 func IsValidFile(mType string) bool {
-	allowed := []string{"text/plain", "image/png", "image/jpeg", "application/word", "application/pdf", "video/mpeg", "video/mp4"}
+	allowed := []string{
+		//image
+		"image/png",
+		"image/jpeg",
+		"image/jpg",
+		"image/gif",
+		"image/bmp",
+		"image/tiff",
+		"image/webp",
+		"image/svg",
+		"image/x-ico",
+		"image/heic",
+		//document
+		"text/plain",
+		"application/word",
+		"application/pdf",
+		//Video
+		"video/mpeg",
+		"video/mp4",
+		"video/x-msvideo",  //avi
+		"video/x-matroska", //mkv
+		"video/quicktime",  //mov
+		"video/x-ms-wmv",   //wmv
+		"video/x-flv",      //flv
+		"video/webm",       //webm
+		"video/mpeg",
+	}
 	return mimetype.EqualsAny(mType, allowed...)
 }
